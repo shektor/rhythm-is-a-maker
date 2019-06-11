bd8d6b7647680ccffad3e673bdc729d2405bf30e
sarpong <abasimisarpong@rocketmail.com>
2019-06-11 16:22:10 +0100

    stored socket.js and tone.js scripts into public

---
+++ public/tone.js
@@ -0,0 +1,78 @@
+(function(){
+  document.documentElement.addEventListener('mousedown', () => {
+    if (Tone.context.state !== 'running') Tone.context.resume();
+  });
+  
+  const synths = [
+    new Tone.Synth(),
+    new Tone.Synth(),
+    new Tone.Synth()
+  ];
+  
+  synths[0].oscillator.type = 'triangle';
+  synths[1].oscillator.type = 'sine';
+  synths[2].oscillator.type = 'sawtooth';
+  
+  const gain = new Tone.Gain(0.8);
+  gain.toMaster();
+  
+  synths.forEach(synth => synth.connect(gain));
+  
+  const $rows = document.body.querySelectorAll('div > div'),
+        notes = ['G5', 'E4', 'C3'];
+  let index = 0;
+  
+  Tone.Transport.scheduleRepeat(repeat, '8n');
+  Tone.Transport.start();
+  
+  function repeat(time) {
+    let step = index % 8;
+    for (let i = 0; i < $rows.length; i++) {
+      let synth = synths[i],
+          note = notes[i],
+          $row = $rows[i],
+          $input = $row.querySelector(`input:nth-child(${step + 1})`);
+      if ($input.checked) synth.triggerAttackRelease(note, '8n', time);
+    }
+    index++;
+  }
+  
+})()
+// document.documentElement.addEventListener('mousedown', () => {
+//     if (Tone.context.state !== 'running') Tone.context.resume();
+//   });
+  
+//   const synths = [
+//     new Tone.Synth(),
+//     new Tone.Synth(),
+//     new Tone.Synth()
+//   ];
+  
+//   synths[0].oscillator.type = 'triangle';
+//   synths[1].oscillator.type = 'sine';
+//   synths[2].oscillator.type = 'sawtooth';
+  
+//   const gain = new Tone.Gain(0.8);
+//   gain.toMaster();
+  
+//   synths.forEach(synth => synth.connect(gain));
+  
+//   const $rows = document.body.querySelectorAll('div > div'),
+//         notes = ['G5', 'E4', 'C3'];
+//   let index = 0;
+  
+//   Tone.Transport.scheduleRepeat(repeat, '8n');
+//   Tone.Transport.start();
+  
+//   function repeat(time) {
+//     let step = index % 8;
+//     for (let i = 0; i < $rows.length; i++) {
+//       let synth = synths[i],
+//           note = notes[i],
+//           $row = $rows[i],
+//           $input = $row.querySelector(`input:nth-child(${step + 1})`);
+//       if ($input.checked) synth.triggerAttackRelease(note, '8n', time);
+//     }
+//     index++;
+//   }
+  
