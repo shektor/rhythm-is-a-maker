76c9417f5ae673be117932269027407239540083
sarpong <abasimisarpong@rocketmail.com>
2019-06-11 16:21:16 +0100

    set up the public directory

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
+  .use(express.static('public'))
   .use((req, res) => res.sendFile(INDEX))
+  .set('view engine', 'ejs')
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const wss = new SocketServer({ server });
