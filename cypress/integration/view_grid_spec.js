9eabdd3593749ecf2ae49f39f7b5765998e9f016
sarpong <abasimisarpong@rocketmail.com>
2019-06-12 12:21:13 +0100

    returned test back to normal

--- cypress/integration/view_grid_spec.js
+++ cypress/integration/view_grid_spec.js
@@ -1,18 +1,5 @@
-describe('My first test', function(){
-  it('Does not do much', function(){
-    expect(true).to.equal(true)
-  })
-})
-
-// describe('My first test', function(){
-//   it('Does not do much', function(){
-//     expect(true).to.equal(false)
-//   })
-// })
-
-it('visits the local host', function(){
- cy.visit('')
-
-  cy.contains('1')
-  cy.get('button').should('have.id', 'd1-1')
-})
+describe('Routing', function(){
+  it('can visit the index page', function(){
+    cy.visit('/')
+   })
+});
