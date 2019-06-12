edcb5f94a819153ff823b8131652c33a0b5e6d8e
sarpong <abasimisarpong@rocketmail.com>
2019-06-12 12:25:59 +0100

    move tone.js to Javascript directory

---
+++ public/javaScript/tone.js
@@ -0,0 +1,7 @@
+var HOST = location.origin.replace(/^http/, 'ws');
+var ws = new WebSocket(HOST);
+var el = document.getElementById('server-time');
+
+ws.onmessage = function (event) {
+  el.innerHTML = 'Server time: ' + event.data;
+};
