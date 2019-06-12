c0252af17614e3c7b70f7da4ba7835c25384b3ab
Jake Napper <napperjlg@gmail.com>
2019-06-12 15:34:47 +0100

    delete references to server time

--- cypress/integration/view_grid_spec.js
+++ cypress/integration/view_grid_spec.js
@@ -2,10 +2,4 @@ describe('Routing', function(){
   it('can visit the index page', function(){
     cy.visit('/')
    })
-
-   it('visits the local host', function(){
-    cy.visit('')
-
-     cy.contains('Rhythm is a Maker')
-   })
 });
--- server.js
+++ server.js
@@ -17,9 +17,3 @@ wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
 });
-
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000);
