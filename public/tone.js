b798a6e7ccac7b1d5dcbc4370d376950e726c2e7
Jake Napper <napperjlg@gmail.com>
2019-06-12 11:05:59 +0100

    synth tones replaced with downloaded drum samples

--- public/tone.js
+++ public/tone.js
@@ -2,29 +2,29 @@
   document.documentElement.addEventListener('mousedown', () => {
     if (Tone.context.state !== 'running') Tone.context.resume();
   });
-  
+
   const synths = [
-    new Tone.Synth(),
-    new Tone.Synth(),
-    new Tone.Synth()
+    new Tone.Player('./sounds/Dry-Kick.wav').toMaster(),
+    new Tone.Player('./sounds/Korg-TR-Rack-Standard-Kit-Snare-Drum.wav').toMaster(),
+    new Tone.Player('./sounds/Closed-Hi-Hat-1.wav').toMaster()
   ];
-  
-  synths[0].oscillator.type = 'triangle';
-  synths[1].oscillator.type = 'sine';
-  synths[2].oscillator.type = 'sawtooth';
-  
+
+  // synths[0].oscillator.type = 'triangle';
+  // synths[1].oscillator.type = 'sine';
+  // synths[2].oscillator.type = 'sawtooth';
+
   const gain = new Tone.Gain(0.8);
   gain.toMaster();
-  
+
   synths.forEach(synth => synth.connect(gain));
-  
+
   const $rows = document.body.querySelectorAll('div > div'),
         notes = ['G5', 'E4', 'C3'];
   let index = 0;
-  
+
   Tone.Transport.scheduleRepeat(repeat, '8n');
   Tone.Transport.start();
-  
+
   function repeat(time) {
     let step = index % 8;
     for (let i = 0; i < $rows.length; i++) {
@@ -32,38 +32,38 @@
           note = notes[i],
           $row = $rows[i],
           $input = $row.querySelector(`input:nth-child(${step + 1})`);
-      if ($input.checked) synth.triggerAttackRelease(note, '8n', time);
+      if ($input.checked) synth.start();
     }
     index++;
   }
-  
+
 })()
 // document.documentElement.addEventListener('mousedown', () => {
 //     if (Tone.context.state !== 'running') Tone.context.resume();
 //   });
-  
+
 //   const synths = [
 //     new Tone.Synth(),
 //     new Tone.Synth(),
 //     new Tone.Synth()
 //   ];
-  
+
 //   synths[0].oscillator.type = 'triangle';
 //   synths[1].oscillator.type = 'sine';
 //   synths[2].oscillator.type = 'sawtooth';
-  
+
 //   const gain = new Tone.Gain(0.8);
 //   gain.toMaster();
-  
+
 //   synths.forEach(synth => synth.connect(gain));
-  
+
 //   const $rows = document.body.querySelectorAll('div > div'),
 //         notes = ['G5', 'E4', 'C3'];
 //   let index = 0;
-  
+
 //   Tone.Transport.scheduleRepeat(repeat, '8n');
 //   Tone.Transport.start();
-  
+
 //   function repeat(time) {
 //     let step = index % 8;
 //     for (let i = 0; i < $rows.length; i++) {
@@ -75,4 +75,3 @@
 //     }
 //     index++;
 //   }
-  
