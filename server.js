13d74129e229b30d4a90c8087a68aa035d565381
sarpong <abasimisarpong@rocketmail.com>
2019-06-12 12:24:36 +0100

    set the view engine to use ejs

--- server.js
+++ server.js
@@ -2,14 +2,12 @@
 
 const express = require('express');
 const SocketServer = require('ws').Server;
-const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
   .use(express.static('public'))
-  .use((req, res) => res.sendFile(INDEX))
+  .use('/', require('./routes/index'))
   .set('view engine', 'ejs')
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
