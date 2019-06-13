120443cce2434522d0685ffe07d17801c4eb329d
sarpong <abasimisarpong@rocketmail.com>
2019-06-13 17:49:19 +0100

    added karma js to run test

---
+++ spec/karma.conf.js
@@ -0,0 +1,39 @@
+// Karma configuration
+// Generated on Thu Jun 13 2019 16:45:55 GMT+0100 (BST)
+
+module.exports = function(config) {
+  var configuration = {
+    basePath: '',
+    plugins: ['karma-chrome-launcher', 'karma-jasmine'],
+    frameworks: ['jasmine'],
+    files: [
+      'https://unpkg.com/tone',
+      '../public/javascript/BeatPlayer.js',
+      '../spec/*Spec.js'
+    ],
+    exclude: [
+    ],
+    preprocessors: {
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+    singleRun: false,
+    concurrency: Infinity
+
+  }
+  
+  if (process.env.TRAVIS) {
+    configuration.browsers = ['Chrome_travis_ci'];
+ }
+ config.set(configuration);
+}
