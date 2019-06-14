78913e188ef2bff789630cc98f181d365315ad68
shektor <44544977+shektor@users.noreply.github.com>
2019-06-14 17:35:35 +0100

    Update view grid feature test
    Test does not currently check the window contains any elements. Add
    assertions that the window contains the title and the first and last
    buttons of a 2 row sound board.

--- cypress/integration/view_grid_spec.js
+++ cypress/integration/view_grid_spec.js
@@ -1,5 +1,12 @@
-describe('Routing', function(){
+describe('sound grid', function(){
   it('can visit the index page', function(){
     cy.visit('/')
-   })
+    cy.contains('Rhythm Is A Maker')
+  });
+
+ it('can see sound grid', function(){
+   cy.visit('/')
+   cy.contains('#button-11')
+   cy.contains('#button-216')
+  })
 });
