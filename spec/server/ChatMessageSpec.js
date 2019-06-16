d65afc2d96f0a6fcbf50848943d4fc4404c51a23
shektor <44544977+shektor@users.noreply.github.com>
2019-06-16 22:59:18 +0100

    Add server chat message handler
    JSON objects will be used to send chat messages between the clients and
    server. A class is required to parse and create JSON objects.

---
+++ spec/server/ChatMessageSpec.js
@@ -0,0 +1,30 @@
+const ChatMessage = require('../../server/ChatMessage.js')
+
+describe('ChatMessage', () => {
+  let chatMessage, clientMessage;
+
+  beforeEach(() => {
+    clientMessage = '{"type":"message","data":"A great message!"}'
+    chatMessage = new ChatMessage(clientMessage)
+  })
+
+  describe('#jsonParse', () => {
+    it('parses a json object', () => {
+      let json = { type: 'message', data: 'A great message!' }
+      expect(chatMessage.jsonParse()).toEqual(json)
+    })
+  })
+
+  describe('#jsonCreate', () => {
+    it('creates a json object', () => {
+      let json = { type: 'message', data: 'A great message!' }
+      expect(chatMessage.jsonParse()).toEqual(json)
+    })
+  })
+
+  describe('#jsonStringify', () => {
+    it('stringifys a json object', () => {
+      expect(chatMessage.jsonStringify()).toEqual(clientMessage)
+    })
+  })
+})
