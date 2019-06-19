c4aeedc5b546637900d25210ee5b573eae0cb831
shektor <44544977+shektor@users.noreply.github.com>
2019-06-19 16:48:51 +0100

    Update test for modified view

--- spec/integration/view_grid_spec.js
+++ spec/integration/view_grid_spec.js
@@ -6,7 +6,6 @@ describe('sound grid', function(){
 
   it('can see sound grid', function(){
    cy.visit('/')
-   cy.get('#button-12').should('be.empty')
-   cy.get('#button-216').should('be.empty')
+   cy.get('#button-12').contains('.')
   })
 });
