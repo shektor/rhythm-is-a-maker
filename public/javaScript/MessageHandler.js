72e875a41ee37b5a4c9da9bce8e65959e1c1ce61
shektor <44544977+shektor@users.noreply.github.com>
2019-06-19 10:14:04 +0100

    Update server and client message handler
    Server should use the new ChatClient and ChatController classes to
    manage users connecting to the site and chatting with each other. The
    message handler on the client side needs logic to correctly handle the
    different types of messages now being sent.

--- public/javaScript/MessageHandler.js
+++ public/javaScript/MessageHandler.js
@@ -2,7 +2,9 @@ const HOST = location.origin.replace(/^http/, 'ws'),
   ws = new WebSocket(HOST),
   content = $('#content'),
   status = $('#status'),
-  input = $('#input');
+  input = $('#input'),
+  clients = $('#clients');
+let userName;
 
 ws.onopen = () => {
   input.removeAttr('disabled');
@@ -10,15 +12,23 @@ ws.onopen = () => {
 };
 
 ws.onmessage = (message) => {
+  console.log(message)
   let json = JSON.parse(message.data)
-  content.prepend('<p>' + json.data + '</p>')
+  if (json.type === 'username') {
+    userName = json.data
+    ws.send(JSON.stringify({ type: 'message', username: userName, data: 'has entered chat!' }))
+  } else if (json.type === 'clients') {
+    clients.html(json.data)
+  } else if (json.type === 'message') {
+    content.prepend('<p>' + json.username + ': ' + json.data + '</p>')
+  }
 }
 
 const chat = new Chat('#input', '#content', $)
 
 chat.inputArea().keydown((key) => {
   if (chat.isKeyCodeEnter(key)) {
-    ws.send(JSON.stringify({ type: 'message', data: chat.getMessage() }))
+    ws.send(JSON.stringify({ type: 'message', username: userName, data: chat.getMessage() }))
     chat.clearInput()
   }
 })
